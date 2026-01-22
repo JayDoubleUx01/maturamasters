a91ade23c8025f555f26f94ea568366984e57bd9
JayDoubleUx01 <kubawx01@gmail.com>
2026-01-22 10:36:15 +0100

    Zmiana ekranu pod mobilki

--- app.py
+++ app.py
@@ -1412,4 +1412,4 @@ def baza_tabela_podglad(table_name):
 # =====================================================
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
